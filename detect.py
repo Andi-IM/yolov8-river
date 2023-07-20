8e026e6609d8d367e8c3aa44e858ee83d4010cb9
Andi-IM <andi.irhamm@gmail.com>
2023-07-20 17:29:34 +0700

    adding text

--- detect.py
+++ detect.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 model = YOLO('best.pt')
 
-image = Image.open('sample.png')
+image = Image.open('full.png')
 
 results = model(image)
 
@@ -19,13 +19,15 @@ boxes = results[0].boxes
 box = boxes[0]
 data = box.xywh
 numpy_data = data.detach().to('cpu').numpy()
+print("numpy data = ", numpy_data)
 detected_height = numpy_data[0][3]
-
+detected_width = numpy_data[0][2]
 print("water_speed = ", speed, " px/s (cuma dipatok doang)")
+print("current_width = ", detected_width, "px")
 print("current_height = ", detected_height, "px")
 estimated_time = (max_height - detected_height) / speed 
 print("estimated time = ", "{:10.2f}".format(estimated_time), " s")
-cv2.imshow("result", plot)
+# cv2.imshow("result", plot)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- video.py
+++ video.py
@@ -8,6 +8,10 @@ model = YOLO('best.pt')
 video_path = 'sample.mp4'
 capture = cv2.VideoCapture(video_path)
 
+speed = 5 # in px/s
+max_height = 1077.2202 # taken from last maximum record
+# max_width = 1559.206
+# max_area = max_width * max_height
 # Loop through the video frames
 while capture.isOpened():
     # read a frame from the video
@@ -19,11 +23,40 @@ while capture.isOpened():
 
         # visualize the results on the frame
         annotated_frame = results[0].plot()
-    
+        boxes = results[0].boxes
+        if (len(boxes) == 0):
+            continue
+        else:
+            box = boxes[0]
+            metrics = box.xywh
+            numpy_data = metrics.detach().to('cpu').numpy()
+            detected_height = numpy_data[0][3]
+            # detected_width = numpy_data[0][2]
+            # detected_area = detected_width * detected_height
+            estimated_time = (max_height - detected_height) / speed 
+
+            if (estimated_time >= 0):
+                time_text = "Estimated time = {:.1f}s".format(estimated_time)
+            else:
+                time_text = "FULL!!"
+
+            annotated_frame = cv2.putText(
+                annotated_frame, 
+                f"Water speed = {speed} px/s (note: this is only constanta)",
+                (0,50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255,0,0), 2, cv2.LINE_AA)    
+            annotated_frame = cv2.putText(
+                annotated_frame, 
+                time_text,
+                (0,80),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255,0,0), 2, cv2.LINE_AA)
+            
         # Display the annotated frame
 
         cv2.imshow("YOLOv8 Inference", annotated_frame)
-
+        
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
